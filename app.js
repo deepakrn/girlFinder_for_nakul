8ff1ca7a1b9d7e5660848838e9e2419ac01f3641
System Administrator <root@amazon.com>
2016-05-22 00:06:04 -0700

    redis to go change

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ var responseController = require('./server/controllers/responseController');
 var FACEBOOK_APP_ID = "1267163483311214";
 var FACEBOOK_APP_SECRET = "01f0836d634e38244d661d8feee83a48";
 
-var redis = require('redis');
-//var client = redis.createClient(10370, "redistogo:8897206b2ab4b13b9425f52be3b729fb@viperfish.redistogo.com"); 
-var client = require('redis').createClient(process.env.REDIS_URL);
+// var redis = require('redis');
+// //var client = redis.createClient(10370, "redistogo:8897206b2ab4b13b9425f52be3b729fb@viperfish.redistogo.com"); 
+// var client = require('redis').createClient(process.env.REDIS_URL);
+
+var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+var client = require("redis").createClient(rtg.port, rtg.hostname);
+
+client.auth(rtg.auth.split(":")[1]);
 //var client = redis.createClient();
 
 //var client = redis.createClient(port, host);
